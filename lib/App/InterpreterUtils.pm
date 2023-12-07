39daf03212f8371151600f8630fc1d3779321fdf
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-12-07 10:49:07 +0700

    Initial commit

---
+++ lib/App/InterpreterUtils.pm
@@ -0,0 +1,22 @@
+package App::InterpreterUtils;
+
+use strict;
+
+# VERSION
+# AUTHORITY
+# DATE
+# DIST
+
+1;
+# ABSTRACT: INI utilities
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+=cut
